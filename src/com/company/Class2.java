a66decf6395a5b7fcb175986f61d5ea768713ce0
dhenirama <ramaadheni@gmail.com>
2021-06-19 13:49:02 +0800

    class1.java

---
+++ src/com/company/Class2.java
@@ -0,0 +1,49 @@
+package com.company;
+
+public class Class2 {
+    public class EntitasSewaHotel {
+        protected int jenis, lama, status;
+        protected String nama, alamat;
+
+        public int getJenis() {
+            return jenis;
+        }
+
+        public void setJenis(int jenis) {
+            this.jenis = jenis;
+        }
+
+        public int getLama() {
+            return lama;
+        }
+
+        public void setLama(int lama) {
+            this.lama = lama;
+        }
+
+        public int getStatus() {
+            return status;
+        }
+
+        public void setStatus(int status) {
+            this.status = status;
+        }
+
+        public String getNama() {
+            return nama;
+        }
+
+        public void setNama(String nama) {
+            this.nama = nama;
+        }
+
+        public String getAlamat() {
+            return alamat;
+        }
+
+        public void setAlamat(String alamat) {
+            this.alamat = alamat;
+        }
+    }
+
+}
